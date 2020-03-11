aee340b734296f6a847cdc2d6586b30e7b637bf1
Watanabe Hiroki <hiroki@WatanabenoMacBook-puro.local>
2020-03-11 14:21:04 +0900

    calc

---
+++ js_basic/js/variable.js
@@ -0,0 +1,27 @@
+var x;
+
+var i;
+var j;
+
+var i, j;
+
+// var str =  '初めてのjs';
+var str =  '初めてのjs';
+
+var k = 100;
+
+var name;
+var _name;
+var $name;
+
+var name1;
+var _name1;
+
+var foo;
+
+var Foo;
+
+
+
+var colorName =  "blue";
+console.log(colorName);
