f3826cbea56b84936b6bbd335fde3954aee0a206
Watanabe Hiroki <hiroki@WatanabenoMacBook-puro.local>
2020-03-11 16:03:14 +0900

    array

--- js_basic/js/branch.js
+++ js_basic/js/branch.js
@@ -1,7 +1,43 @@
-var x = 100;
+// var x = 100;
 
-if (x >= 100) {
-  console.log('100以上の値です');
-} else{
-  console.log('100未満の値です')
+// if (x >= 100) {
+//   console.log('100以上の値です');
+// } else{
+//   console.log('100未満の値です')
+// }
+
+// var y = 90;
+
+// if (y >= 90) {
+//   console.log('A');
+// }else if (y >= 80) {
+//   console.log('B');
+// } else if (y >= 60){
+//   console.log('C')
+// } else {
+//   console.log('D');
+// }
+
+
+var n = 4;
+
+switch(n){
+
+  case 1:
+    console.log('1です');
+    break;
+  case 2:
+    console.log('2です');
+    break;
+  case 3:
+    console.log('3です');
+    break;
+  default:
+    console.log('範囲外');
+    break;
 }
+
+
+
+
+
